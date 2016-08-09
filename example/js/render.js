62c2019426a02baba229f465cd0765d7badcd510
the_input <chjalmar@gmail.com>
2016-08-09 12:37:31 -0400

    Definida promesa en render.js para descargar y leer el json primero, y iniciar el lightbox después

--- example/js/render.js
+++ example/js/render.js
@@ -1,5 +1,10 @@
  $(document).ready(function(){
-   $.get('alexis100.json', function(data) {
+    getFile.then(iniciargaleria);
+ });
+
+var getFile = new Promise(function(resolve, reject){
+	
+  $.get('alexis100.json', function(data) {
        for (var i =0; i < data.length;i++) {
        	 var description = data[i].Desc;
        	 var fecha = description.split('.')[0];
@@ -7,12 +12,12 @@
          var html = '<li data-src="img/fullscreen/'+data[i].Imagen+'" data-sub-html="<h4>'+fecha+'</h4> '+descriptionSinFecha+'" style="background-image: url(img/thumbnails/'+data[i].Imagen+');"><a><img src="img/thumbnails/'+data[i].Imagen+'"></a> </li>';
          //if (i < 10) {
          $("#aniimated-thumbnials").append(html);
-     	//}
-         //console.log(html);
-       }
-    });
-   	//setTimeout ('iniciargaleria()', 100);
- });
+     	 }
+  
+       resolve(true);
+  });
+
+});
 
 
 function iniciargaleria() {
